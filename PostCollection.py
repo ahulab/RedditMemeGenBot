bf25952e352fd44c120a869bf8085a973ac09142
ahulab <alex@alexbaluha.com>
2015-08-18 19:14:41 -0400

    Woooh!
    Added some guesswork so that images with long comments have big enough
    text so that the image doesn’t just have one long line of text across
    the top and bottom that is too small to read

--- PostCollection.py
+++ PostCollection.py
@@ -12,7 +12,8 @@ def add_text(image, post_id, comment):
 
 	##if i need to try sanitizing text
 	# meme_string = meme_string.rstrip('\r\n')
-	comment = re.sub(r'[\n\r]', '', comment)
+	comment = re.sub(r'[\n\r]', '', comment).encode('utf-8')
+
 
 	#get both halfs of the string
 	string_halves = halve_string(comment)
@@ -20,10 +21,28 @@ def add_text(image, post_id, comment):
 	print "First half string length: {}. Second half length: {}".format(len(string_halves[0]), len(string_halves[1]))
 	#choosing font size
 
+	word_count = 0
+	for i in string_halves[0]:
+		if i == " ":
+			word_count += 1
+	for i in string_halves[1]:
+		if i == " ":
+			word_count += 1
+
+	print word_count
 	fnt = font_setup(string_halves, image.width, 10)
 
-	#dirty, gives us x lines of text so that text isn't super small on image
-	lines = 1
+	#some guesswork. depending on the number of words we are going to make the image have x lines of text
+	#this is so that images with long comments do not just have one long line of unlegible text across the top and bottom
+	if word_count < 20:
+		lines = 1
+	elif word_count > 20 and word_count < 60:
+		lines = 3
+	elif word_count > 60 < 150:
+		lines = 5
+	else:
+		lines = 6
+
 	new_size = fnt.size * lines
 	fnt = ImageFont.truetype(path_to_font, size=new_size)
 
@@ -48,7 +67,7 @@ user_agent = "Meme generator bot 0.1 by /u/cDoubt"
 r = praw.Reddit(user_agent=user_agent)
 
 pics_subreddit = r.get_subreddit('pics')
-hot_posts = pics_subreddit.get_hot()
+hot_posts = pics_subreddit.get_top_from_year(limit=50)
 
 #gets 25 'hot' submissions in the subreddit
 pics_posts = []
